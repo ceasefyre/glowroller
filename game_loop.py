4c0a462b5cbe7baac4924beafa06ee546f1ad789
Da3MoNiCuS <ricej3@mail.uc.edu>
2016-09-29 13:34:56 -0700

    add 2 buttons and cleanup for testing

--- game_loop.py
+++ game_loop.py
@@ -10,25 +10,38 @@ GPIO.setmode(GPIO.BCM)
 
 #Assign the buttons to GPIO pins
 #NOTE: do NOT duplicate pins across buttons
-btn1 = 17
-btn2 = 18
+btn_pins = dict(
+    'btn1' = 17,
+    'btn2' = 18,
+    'btn3' = 27,
+    'btn4' = 23
+)
 
-#set the callbacks for the interrupts
-options = {
-    btn1 : 
-}
+#button bounceback time for callback triggers
+btn_bounce = 300
 
-running = True;
+#
+running = True
 
-GPIO.setup(btn1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(btn2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-def tst_callback(channel):
+def cb_test(channel):
     print "Button Press (Falling Switch) detected on channel " + str(channel)
 
+def cb_cease_polling(channel):
+    tst_callback(channel)
+    running = False
 
-GPIO.add_event_detect(btn1, GPIO.FALLING, callback=tst_callback, bouncetime=300)
-GPIO.add_event_detect(btn2, GPIO.FALLING, callback=tst_callback, bouncetime=300)
+#set the callbacks for the interrupts
+btn_cb = dict(
+    btn_pins['btn1'] : cb_test,
+    btn_pins['btn2'] : cb_test,
+    btn_pins['btn3'] : cb_test,
+    btn_pins['btn4'] : cb_cease_polling
+)
+
+#iterate the pins dict and set them up with their callbacks
+for k,v in btn_pins
+    GPIO.setup(v, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_even_detect(v, GPIO.FALLING, callback=btn_cb[v], bouncetime=btn_bounce)
 
 try:
     while(running):
