3c0a90627b381d14cd0e2a7297526f736ad00184
Da3MoNiCuS <ricej3@mail.uc.edu>
2016-10-01 19:34:35 -0700

    fixed

--- effects/lib1.py
+++ effects/lib1.py
@@ -11,12 +11,12 @@ class chase(controller.effect):
         i += 1
         
         numLEDs = 64
-        while super(chase, self).running:
+        while self.running:
             while i>0:
                 i -= 1
                 print "running effect " + self.name + " for iteration# " + i
                 for n in range(numLEDs):
                     pixels = [ (0,0,0) ] * numLEDs
                     pixels[n] = (255, 255, 255)
-                    super(chase, self).client.put_pixels(pixels)
+                    self.client.put_pixels(pixels)
                     time.sleep(0.01)
