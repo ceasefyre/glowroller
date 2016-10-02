f9fa3f72337d26f8a360f3f9b96f5e75dbbd955d
Da3MoNiCuS <ricej3@mail.uc.edu>
2016-10-01 19:33:44 -0700

    misnamed variable

--- effects/lib1.py
+++ effects/lib1.py
@@ -11,7 +11,7 @@ class chase(controller.effect):
         i += 1
         
         numLEDs = 64
-        while super(chase, self).isRunning:
+        while super(chase, self).running:
             while i>0:
                 i -= 1
                 print "running effect " + self.name + " for iteration# " + i
